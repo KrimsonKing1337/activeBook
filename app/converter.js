92eda0c1b7375cde00c5ef9caa72fa02628970ca
Oleg Shilov <shilov-1@yandex.ru>
2017-04-24 09:34:10 +0300

    method get formats by type files; used and unused files is set in converterInit, just once;

--- app/converter.js
+++ app/converter.js
@@ -22,118 +22,35 @@ module.exports = class Converter {
 
     /**
      *
-     * @param type {string};
-     * @param [callback] {function};
      *
      * приватный метод,
      * удаляет ненужные файлы,
      * т.е. не .ogg и не .mp3 (или не .webm или .mp4, etc.) после отработки convert
      */
-    _removeUnused (type, callback) {
+    _removeUnused () {
         let files = this.files;
 
         if (files.length === 0) throw 'There are no files, exit';
 
-        let formats;
-        if (type === 'audio') {
-            formats = ['mp3', 'ogg'];
-        } else if (type === 'video') {
-            formats = ['mp4', 'webm']
-        } else {
-            throw 'There is incorrect type. Only audio or video is allowed';
-        }
-
-        /**
-         *
-         * массив нужных файлов,
-         * его мы потом используем для формирования
-         * массива ненужных файлов
-         */
-        let usedFiles = [];
-
-        /**
-         * массив ненужных файлов,
-         * приравниваем его к общему списку файлов,
-         * потом из него будем удалять элементы,
-         * которые есть в массиве нужных файлов
-         */
-        let unUsedFiles = files;
-
-        files.map(function (file) {
-            /**
-             *
-             * вызываем статический метод через название класса,
-             * а не экземпляра класса.
-             * то есть `ClassName.staticMethod()`,
-             * а не `new ClassName.staticMethod()`
-             */
-            let fileProps = Files.getFileProps(file);
-
-            /**
-             * пробегаемся по каждому формату.
-             * если расширение файла = формату
-             * добавляем его в массив нужных файлов
-             */
-            formats.map(function (format, index) {
-                if (fileProps.ext === '.' + format) {
-                    usedFiles.push(file);
-                }
-            });
-        });
-
-        /**
-         * пробегаемся по массиву нужных файлов.
-         * ищем значение каждого элемента массива
-         * в общем списке файлов.
-         * если таковой есть - удаляем его из массива ненужных файлов.
-         * в конечном счёт у нас окажуются только ненужные файлы,
-         * которые мы потом отправляем на удаление
-         */
-        usedFiles.map(function (file) {
-            for (let i = 0; i < unUsedFiles.length; i++) {
-                if (unUsedFiles[i] === file) {
-                    unUsedFiles.splice(i, 1);
-                }
-            }
-        });
-
-        filesInst.remove(unUsedFiles);
+        filesInst.remove(files.unUsedFiles);
     };
 
     /**
      *
-     * @param type {string}; audio || video;
+     * @param formats[] {string};
      * @param [callback] {function};
      *
      * публичный метод,
      * конвертирует в mp3/ogg, mp4/webm
      */
-    convert (type, callback) {
+    convert (formats, callback) {
         let self = this;
         let files = this.files;
 
         if (files.length === 0) throw 'There are no files, exit';
+        if (formats.length === 0) throw 'There are no formats, exit';
 
-        let formats;
-        if (type === 'audio') {
-            formats = ['mp3', 'ogg'];
-        } else if (type === 'video') {
-            formats = ['mp4', 'webm']
-        } else {
-            throw 'There is incorrect type. Only audio or video is allowed';
-        }
-
-        let filesAllowed = [];
-
-        files.map(function (file) {
-            let fileProps = Files.getFileProps(file);
-
-            formats.map(function (format, index) {
-                if (fileProps.ext === '.' + format) {
-                    filesAllowed.push(file);
-                }
-            });
-        });
+        let usedFiles = files.usedFiles;
 
         /**
          *
@@ -142,7 +59,7 @@ module.exports = class Converter {
          */
         let newFiles = [];
 
-        filesAllowed.map(function (file) {
+        usedFiles.map(function (file) {
             /**
              *
              * вызываем статический метод через название класса,
@@ -158,7 +75,7 @@ module.exports = class Converter {
              * добавляем его в массив новых файлов
              */
             formats.map(function (format, index) {
-                if (files.indexOf(fileProps.fullNameWithoutExt + '.' + format) === -1) {
+                if (usedFiles.indexOf(fileProps.fullNameWithoutExt + '.' + format) === -1) {
                     newFiles.push({fileProps: fileProps, format: format});
                 }
             });
@@ -197,7 +114,7 @@ module.exports = class Converter {
                          * подчищаем за собой,
                          * удаляем ненужные файлы (НЕ mp3/ogg или НЕ webm/mp4, etc.)
                          */
-                        self._removeUnused(type);
+                        self._removeUnused(files);
                     }
                 });
             });
@@ -219,6 +136,8 @@ module.exports = class Converter {
      * но поскольку нужно объединить эту функцию в один контекст, мы вносим её в класс в виде метода.
      */
     static ffmpeg (fileProps, format, callback) {
+        console.log('Converting ' + fileProps.fullName + ' to ' + fileProps.dirPath + '/' + fileProps.nameWithoutExt + '.' + format);
+
         ffmpeg(fileProps.fullName).toFormat(format).saveToFile(fileProps.dirPath + '/' + fileProps.nameWithoutExt + '.' + format)
             .on('end', function () {
                 callback(null);
@@ -227,4 +146,24 @@ module.exports = class Converter {
                 callback(err);
             });
     };
+
+    /**
+     *
+     * @param type {string}; audio || video
+     *
+     * статичный метод,
+     * получаем форматы по типу файлов
+     */
+    static getFormatsByType (type) {
+        let formats;
+        if (type === 'audio') {
+            formats = ['mp3', 'ogg'];
+        } else if (type === 'video') {
+            formats = ['mp4', 'webm']
+        } else {
+            throw 'There is incorrect type. Only audio or video is allowed';
+        }
+
+        return formats;
+    }
 };
