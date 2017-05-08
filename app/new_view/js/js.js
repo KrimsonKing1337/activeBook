688f5999161dc79724a931bcd1c5031de844feac
Shilov <shilov-1@yandex.ru>
2017-05-08 14:55:40 +0300

    new view styles;

--- app/new_view/js/js.js
+++ app/new_view/js/js.js
@@ -6,5 +6,27 @@ $(window).load(function () {
         mouseWheel:{ scrollAmount: 75 }
     });
 
-    $('.js-menu-volume')
+    //ionRangeSlider
+    $('.js-range-slider').ionRangeSlider({
+        min: 0,
+        max: 100,
+        from: 50,
+        hide_min_max: true,
+        hide_from_to: true
+    });
+
+    //отображаем доп. меню для звука при нажатии правой кнопкой мыши
+    $('.js-menu-volume').find('> img').on('contextmenu', function (e) {
+       e.preventDefault();
+
+       let $addSettings = $(this).closest('.js-menu-volume').find('.add-settings');
+
+       $addSettings.toggleClass('active');
+
+       setTimeout(function () {
+           $(document).one('click', function () {
+               $addSettings.removeClass('active');
+           });
+       }, 0);
+    });
 });
