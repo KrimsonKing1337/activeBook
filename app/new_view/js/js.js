591dc6cd9eee4388f922524c030b138e302b2141
Oleg Shilov <shilov-1@yandex.ru>
2017-05-16 10:29:23 +0300

    view for popover;

--- app/new_view/js/js.js
+++ app/new_view/js/js.js
@@ -15,37 +15,39 @@ $(window).load(function () {
         hide_from_to: true
     });
 
-    //отображаем доп. меню для звука при нажатии правой кнопкой мыши
-    $('.js-menu-volume').find('.obj-img__wrapper').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+    //отображаем доп. меню для элементов с поповером
+    $('.menu__item').has('.add-settings').each(function (index, item) {
+        $(item).find('.obj-img__wrapper').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
-        let $objImgWrapper = $(this);
-        let $addSettings = $objImgWrapper.closest('.js-menu-volume').find('.add-settings');
-        $addSettings.toggleClass('active');
-        $objImgWrapper.toggleClass('active');
+            let $objImgWrapper = $(this);
+            let $addSettings = $(item).find('.add-settings');
+            $addSettings.toggleClass('active');
+            $objImgWrapper.toggleClass('active');
 
-        /**
-         * убираем всплытие события клик у поповера,
-         * чтобы он не закрывался при нём
-         */
-        setTimeout(function () {
-            $addSettings.on('click', function (e) {
-                e.stopPropagation();
-            });
-        }, 0);
+            /**
+             * убираем всплытие события клик у поповера,
+             * чтобы он не закрывался при нём
+             */
+            setTimeout(function () {
+                $addSettings.on('click', function (e) {
+                    e.stopPropagation();
+                });
+            }, 0);
 
-        /**
-         * клик в любом месте документа,
-         * кроме самого этого элемента
-         * скроет поповер
-         */
-        setTimeout(function () {
-            $(document).one('click', function () {
-                $addSettings.removeClass('active');
-                $addSettings.off('click');
-                $objImgWrapper.removeClass('active');
-            });
-        }, 0);
+            /**
+             * клик в любом месте документа,
+             * кроме самого этого элемента
+             * скроет поповер
+             */
+            setTimeout(function () {
+                $(document).one('click', function () {
+                    $addSettings.removeClass('active');
+                    $addSettings.off('click');
+                    $objImgWrapper.removeClass('active');
+                });
+            }, 0);
+        });
     });
 });
