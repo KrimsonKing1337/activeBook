45dd3fdb812a2ea5b7a05b67d59b74240062d762
Oleg Shilov <shilov-1@yandex.ru>
2018-04-07 12:27:01 +0300

    bowser change statement;

--- src/common/js/browserCheck.js
+++ src/common/js/browserCheck.js
@@ -4,7 +4,8 @@ import bowser from 'bowser';
 export function browserCheck () {
     const $body = $('body');
 
-    if (!bowser.blink && !bowser.gecko && !bowser.safari) {
+    //if (bowser.msie || bowser.msedge || bowser.safari) {
+    if (bowser.msie || bowser.msedge) {
         $body.empty();
         $body.append('<div class="warning">Пожалуйста, используйте Google Chrome версии 58+ или Firefox версии 53+</div>');
 
