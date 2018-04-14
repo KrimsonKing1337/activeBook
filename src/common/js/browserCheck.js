2ec96e55091fca9a5d87f1ab1e7d8ece3beea19f
Oleg Shilov <shilov-1@yandex.ru>
2018-04-14 10:46:57 +0300

    change message about browser is not compatible;

--- src/common/js/browserCheck.js
+++ src/common/js/browserCheck.js
@@ -7,7 +7,7 @@ export function browserCheck () {
     //if (bowser.msie || bowser.msedge || bowser.safari) {
     if (bowser.msie || bowser.msedge) {
         $body.empty();
-        $body.append('<div class="warning">Пожалуйста, используйте Google Chrome версии 58+ или Firefox версии 53+</div>');
+        $body.append('<div class="warning">Пожалуйста, используйте Google Chrome, Safari или Firefox последней актуальной версии</div>');
 
         return false;
     }
