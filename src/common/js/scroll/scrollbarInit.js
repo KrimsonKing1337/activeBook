0c0c0080f3dfb0d892e8939939ebcdfa910e7bb2
Oleg Shilov <shilov-1@yandex.ru>
2019-06-01 18:45:06 +0300

    scrollbar init fix;

--- src/common/js/scroll/scrollbarInit.js
+++ src/common/js/scroll/scrollbarInit.js
@@ -5,56 +5,69 @@ import 'jquery.scrollbar';
  * @param $item {object} jquery
  */
 function scrollbarInit($item) {
-    return new Promise(((resolve, reject) => {
-        $item.scrollbar({
-            onInit() {
-                resolve();
-            }
-        });
-    }));
+  return new Promise(((resolve) => {
+    $item.scrollbar({
+      onInit() {
+        resolve();
+      }
+    });
+  }));
 }
 
 function getScrollableItems() {
-    return $('.js-scrollable-item')
-        .filter(function () {
-            return $(this).hasClass('template') === false && $(this).closest('.template').length === 0;
-        });
+  return $('.js-scrollable-item').filter((i, item) => {
+    const $scrollableCur = $(item);
+
+    /**
+     * строчка $(item).find('> .js-scrollable-item').length === 0
+     * нужна для того, чтобы в выборку не попали элементы,
+     * которые генерирует сам плагин для своей работы.
+     * иначе он пытается и к ним применить себя, в итоге
+     * это приводит к непредсказуемому результату
+     */
+
+    return $scrollableCur.hasClass('template') === false
+      && $scrollableCur.closest('.template').length === 0
+      && $(item).find('> .js-scrollable-item').length === 0;
+  });
 }
 
 export async function scrollbarInitAll() {
-    const promisesArr = [];
+  const promisesArr = [];
 
-    getScrollableItems().each((i, item) => {
-        const $scrollableItemCur = $(item);
+  getScrollableItems().each((i, item) => {
+    const $scrollableItemCur = $(item);
 
-        $scrollableItemCur.addClass('scrollbar-macosx');
+    $scrollableItemCur.addClass('scrollbar-macosx');
 
-        promisesArr.push(scrollbarInit($scrollableItemCur));
-    });
+    promisesArr.push(scrollbarInit($scrollableItemCur));
+  });
 
-    await Promise.all(promisesArr);
+  await Promise.all(promisesArr);
 }
 
 export function scrollbarDestroy() {
-    getScrollableItems().removeClass('scrollbar-macosx').scrollbar('destroy');
+  getScrollableItems()
+    .removeClass('scrollbar-macosx')
+    .scrollbar('destroy');
 }
 
 export function showHideScrollbarTouchEventsFix() {
-    let timer;
+  let timer;
 
-    getScrollableItems().on('touchstart touchmove', function () {
-        if (timer) clearTimeout(timer);
+  getScrollableItems().on('touchstart touchmove', function () {
+    if (timer) clearTimeout(timer);
 
-        $('.touchend').removeClass('touchend');
-        $(this).addClass('touching');
-    });
+    $('.touchend').removeClass('touchend');
+    $(this).addClass('touching');
+  });
 
-    getScrollableItems().on('touchend', function () {
-        if (timer) clearTimeout(timer);
+  getScrollableItems().on('touchend', function () {
+    if (timer) clearTimeout(timer);
 
-        timer = setTimeout(() => {
-            $('.touching').removeClass('touching');
-            $(this).addClass('touchend');
-        }, 300);
-    });
+    timer = setTimeout(() => {
+      $('.touching').removeClass('touching');
+      $(this).addClass('touchend');
+    }, 300);
+  });
 }
